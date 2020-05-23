012ce4b9f258433d18b4e79f550ec129dc6a3510
Mauro Caredda <mauro.dev88@gmail.com>
2020-05-23 16:14:57 +0200

    Update install_pkg_to_current_folder.sh

--- install_pkg_to_current_folder.sh
+++ install_pkg_to_current_folder.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 for f in *.zip
 do
-    echo "installing: $f"
+    echo "installing pkg : $f"
     curl -u admin:admin -F file=@"$f" -F name="$f" -F force=true -F install=true http://localhost:port/crx/packmgr/service.jsp
-    echo "done."
+    echo "Pacchetti caricati."
 
 done
