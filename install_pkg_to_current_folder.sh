d4443c18120b9d5b246c7b3d8b88e8a032be68e4
Mauro Caredda <mauro.dev88@gmail.com>
2020-05-23 16:13:19 +0200

    Update install_pkg_to_current_folder.sh

--- install_pkg_to_current_folder.sh
+++ install_pkg_to_current_folder.sh
@@ -1,10 +1,9 @@
 
 #!/bin/bash
-# this script will install ALL zip packages in current directory the AEM instance at 4502
 for f in *.zip
 do
     echo "installing: $f"
-    curl -u admin:admin -F file=@"$f" -F name="$f" -F force=true -F install=true http://localhost:4502/crx/packmgr/service.jsp
+    curl -u admin:admin -F file=@"$f" -F name="$f" -F force=true -F install=true http://localhost:port/crx/packmgr/service.jsp
     echo "done."
 
 done
