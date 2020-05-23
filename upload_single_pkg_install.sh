bc85a90c12b70cbb9486ebb670377eaec723111c
Mauro Caredda <mauro.dev88@gmail.com>
2020-05-23 16:25:15 +0200

    Update upload_single_pkg_install.sh

--- upload_single_pkg_install.sh
+++ upload_single_pkg_install.sh
@@ -1,4 +1,6 @@
 #!bin/bash
 #Upload a package AND install
+#For upload only change install=false
+change install=false
 curl -u admin:admin -F file=@"name of zip file" -F name="name of package" 
 -F force=true -F install=true http://localhost:4505/crx/packmgr/service.jsp
